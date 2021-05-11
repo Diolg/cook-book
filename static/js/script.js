526e3db4c26a6189f013b8e18a1e90e982f6ff19
Diolg <olga.diakoff@gmail.com>
2021-05-11 21:41:05 +0000

    Add search functionality to home.htm;

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,12 @@
     $('.modal').modal();
     $("select").formSelect();
   }); 
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+let sidenavs = document.querySelectorAll(".sidenav");
+let sidenavsInstance = M.Sidenav.init(sidenavs);
+});
