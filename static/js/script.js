0a4310079522108ff6afc428398f8916da330dd1
Diolg <olga.diakoff@gmail.com>
2021-05-18 17:41:52 +0000

    Remove add event listener code

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,10 @@
  $(document).ready(function(){
     $('.sidenav').sidenav();
-    $('.sidenav').sidenav();
     $(".collapsible").collapsible();
     $('.modal').modal();
     $("select").formSelect();
   }); 
 
 
-
-
-
-    document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-  });
 
 
